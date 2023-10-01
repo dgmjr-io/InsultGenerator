f10f283b874cf5bd2fe406c5291acc8d47296f66
David G. Moore, Jr <david@dgmjr.io>
2023-10-01 05:47:52 -0400

    Run csharpier

--- Program.cs
+++ Program.cs
@@ -5,13 +5,22 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.AddTheWorks(new StartupParametersBuilder()
-    .WithThisAssemblyProject(typeof(ThisAssembly.Project))
-    .WithAzureAppConfiguration(
-        config => config.Connect(builder.Configuration["AZURE_APPCONFIGURATION_CONNECTION_STRING"]),
-        config => config.SetCredential(AddAzureAppConfigExtensions.DefaultKeyVaultCredentialsFactory(builder.Configuration)))
-    .WithHealthChecks(_ => { })
-    .Build());
+builder.AddTheWorks(
+    new StartupParametersBuilder()
+        .WithThisAssemblyProject(typeof(ThisAssembly.Project))
+        .WithAzureAppConfiguration(
+            config =>
+                config.Connect(builder.Configuration["AZURE_APPCONFIGURATION_CONNECTION_STRING"]),
+            config =>
+                config.SetCredential(
+                    AddAzureAppConfigExtensions.DefaultKeyVaultCredentialsFactory(
+                        builder.Configuration
+                    )
+                )
+        )
+        .WithHealthChecks(_ => { })
+        .Build()
+);
 
 var app = builder.Build();
 
